3f43c25f628a4874df13eb6835b7daf7fcee28d1
EnioAmarantes <enio.amarantes@gmail.com>
2022-04-29 10:29:37 -0300

    Ajustes na tela de login e cadastro

--- src/pages/login/CreateLogin.js
+++ src/pages/login/CreateLogin.js
@@ -1,10 +1,90 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
+import {TextInput} from 'react-native-paper';
+import { Button } from "@react-native-material/core";
 
-export default function CreateLogin(props) {
+const CreateLogin = (props) => {
+    const [email, onChangeEmail] = React.useState('');
+    const [pass, onChangePass] = React.useState('');
+    const [hidePass, setHidePass] = React.useState(true);
+    const [confirmPass, onChangeConfirmPass] = React.useState('');
+    const [hideConfirmPass, setHideConfirmPass] = React.useState(true);
+
     return(
-        <View>
-            <Text>Tela de Cadastro</Text>
+        <View style={styles.container}>
+
+            <Text style={styles.text}>Preencha os dados do seu cadastro</Text>
+
+            <View style={styles.inputContainer}>
+
+                <TextInput style={styles.input} label="E-mail" keyboardType='email-address' onChangeText={onChangeEmail}  />
+
+                <TextInput 
+                    style={styles.input}
+                    label="Senha" 
+                    secureTextEntry={hidePass} 
+                    value={pass}
+                    onChangeText={onChangePass} 
+                    right={<TextInput.Icon name={hidePass ? "eye" : "eye-off"} onPress={() => setHidePass(!hidePass)}  />}
+                />
+
+                
+                <TextInput 
+                    style={styles.input}
+                    label="Repetir Senha" 
+                    secureTextEntry={hideConfirmPass} 
+                    value={confirmPass}
+                    onChangeText={onChangeConfirmPass} 
+                    right={<TextInput.Icon name={hideConfirmPass ? "eye" : "eye-off"} onPress={() => setHideConfirmPass(!hideConfirmPass)}  />}
+                />
+
+                {
+                    ((pass === '' || confirmPass === '') || pass === confirmPass)
+                    ? null 
+                    : <Text style={styles.diferentPass}>Senha não Confere</Text>
+                }
+
+                <Button 
+                    onPress={() => props.navigation.navigate('login')} 
+                    style={styles.buttonCadastrar} 
+                    title="CADASTRAR" 
+                    color="#6A61A1"
+                />
+            </View>
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#43405E",
+        height: '100%',
+    },
+    text: {
+        fontSize: 28,
+        color: "#fff",
+        textAlign: 'center',
+        marginTop: 80,
+        fontFamily: 'PSL Ornanong Pro',
+    },
+    inputContainer :{
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 35,
+    },
+    input:{
+        marginVertical: 6,
+    },
+    buttonCadastrar : {
+        height: 45,
+        justifyContent: 'center',
+        fontSize: 14,
+        marginTop: 25
+    },
+    diferentPass: {
+        color: 'red',
+        marginLeft: 15,
+    }
+});
+
+export default CreateLogin;
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,104 @@
 import React from "react";
-import {View, Text, Button} from 'react-native';
+import { View, Text, StyleSheet, Image } from "react-native";
+import {TextInput} from 'react-native-paper';
+import { Button } from "@react-native-material/core";
 
-export default function Login(props) {
+import Mind from '../../../assets/imgs/mind.png';
+
+const Login = (props) => {
+    const [email, onChangeEmail] = React.useState('');
+    const [pass, onChangePass] = React.useState('');
+    const [hidePass, setHidePass] = React.useState(true);
+
     return(
-        <View>
-            <Text>Tela de Login</Text>
-            <Button title="Entrar" onPress={() => props.navigation.navigate('menu')} />
+        <View style={styles.container}>
+            <Image style={styles.img} source={Mind} />
+
+            <Text style={styles.textHeader}>Mind Booster</Text>
+
+            <View style={styles.inputContainer}>
+                    <TextInput style={styles.input} label="E-mail" keyboardType='email-address' />
+
+                    <TextInput 
+                        style={styles.input}
+                        label="Senha" 
+                        secureTextEntry={hidePass} 
+                        value={pass}
+                        onChangeText={onChangePass} 
+                        right={<TextInput.Icon name={hidePass ? "eye" : "eye-off"} onPress={() => setHidePass(!hidePass)}  />}
+                    />
+
+                    <Text style={styles.text}>Esqueci a senha</Text>
+
+                    <View style={styles.buttonContainer}>
+                        <Button 
+                            style={styles.buttonLogin} 
+                            onPress={() => { props.navigation.navigate('menu') }} 
+                            title="ENTRAR" 
+                            color="#6A61A1"
+                        />
+
+                        <Button 
+                            style={styles.buttonCadastrar} 
+                            titleStyle={{color: '#fff'}} 
+                            title="CADASTRE-SE" 
+                            onPress={() => { props.navigation.navigate('createLogin') }} 
+                        />
+                    </View>
+            </View>
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#43405E",
+        height: '100%',
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+    },
+    img:{
+        width: 132,
+        height: 132,
+        marginTop: 20,
+        alignSelf: 'center'
+    },
+    textHeader: {
+        fontSize: 45,
+        color: "#fff",
+        textAlign: 'center',
+        fontFamily: 'Pacifico', 
+    },
+    inputContainer :{
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+    },
+    input:{
+        marginVertical: 10,
+    },
+    buttonContainer: {
+        justifyContent:'space-between',
+        height: '40%',
+    },
+    buttonLogin: {
+        color: '#6A61A1',
+        fontSize: 14,
+        minHeight: 45,
+        justifyContent: 'center',
+    },
+    buttonCadastrar : {
+        fontSize: 14,
+        backgroundColor: '#B58D97',
+        minHeight: 45,
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 14,
+        color: "#fff",
+        textAlign: 'right',
+        marginBottom: 20
+    }
+});
+
+export default Login;
